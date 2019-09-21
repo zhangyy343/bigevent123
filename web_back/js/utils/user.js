dbf7c65f58417c85fa6dcf06fbe9911a57ce0f28
zhangyy343 <429110621@qq.com>
2019-09-21 21:34:40 +0800

    管理员退出功能

--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -3,6 +3,11 @@ var user={
         // alert('login');
         $.post('http://localhost:8000/admin/login',{user_name:userName,password:password}, function(res){
             callback(res);
-        }
-        )}
+         });
+    },
+    logout:function(callback){
+        $.post('http://localhost:8000/admin/logout',function(res){
+            callback(res);
+        })
     }
+    };

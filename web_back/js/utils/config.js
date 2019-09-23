331b6a747edbb916e76442cc9cf3fbb3b1ba8ee1
zhangyy343 <429110621@qq.com>
2019-09-23 15:54:21 +0800

    文章的类别查询

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -2,5 +2,7 @@ var BASE_URL= "http://localhost:8000"
 var URLIST = {
     user_login: BASE_URL+'/admin/login',
     user_logout:BASE_URL+'/admin/logout',
-    user_info: BASE_URL+'/admin/getuser'
+    user_info: BASE_URL+'/admin/getuser',
+
+    category_show: BASE_URL+'/admin/category_search'
 };
--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -1,7 +1,15 @@
+//管理员模块：所有与管理员相关的功能都写在这里
 var user={
+    /**
+     * 
+     * @param {*} userName 用户名
+     * @param {*} password 密码
+     * @param {*} callback 回调函数
+     */
     login:function(userName,password,callback){
         // alert('login');
         $.post(URLIST.user_login,{user_name:userName,password:password}, function(res){
+            //res 是本次请求，从服务器回来的数据
             callback(res);
          });
     },

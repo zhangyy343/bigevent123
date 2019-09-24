da6c2583f9cd99d114b2f4798dba765aa9328eca
zhangyy343 <429110621@qq.com>
2019-09-24 21:59:29 +0800

    编辑文章分类

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -4,5 +4,8 @@ var URLIST = {
     user_logout:BASE_URL+'/admin/logout',
     user_info: BASE_URL+'/admin/getuser',
 
-    category_show: BASE_URL+'/admin/category_search'
+    category_show: BASE_URL+'/admin/category_search',
+    category_add:BASE_URL+'/admin/category_add',
+    category_del:BASE_URL+'/admin/category_delete',
+    category_edit:BASE_URL+'/admin/category_edit'
 };
